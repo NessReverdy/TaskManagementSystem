7b19353c37994abd9d5eb65b4f85580331e2b4ce
Anastasia <winchester726@mail.ru>
2026-07-13 23:30:00 +0300

    #14 - init project-service;

---
+++ project-service/src/main/java/org/nessrev/projectservice/ProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package org.nessrev.projectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectServiceApplication.class, args);
+  }
+
+}
